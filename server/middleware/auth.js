6884636e0699908d86bde05584110608f4e12e5e
kaytee07 <kofi.taylor07@gmail.com>
2024-02-01 15:55:57 +0000

    authorization feature implemented

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,9 +1,15 @@
 import jwt from "jsonwebtoken";
 
-const verifyToken = (req, res, next) => {
+export const verifyToken = (req, res, next) => {
     try {
+        const token = req.cookies.jwt;
+        if (!token) return res.status(400).json({ msg: "Access Denied" });
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next()
 
     } catch (err) {
-        
+        res.status(500).json({ msg: err });
     }
 }
