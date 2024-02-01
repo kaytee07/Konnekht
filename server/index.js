8b0e78b9815c16ad06f6447b25dca7f3fb9dc33a
kaytee07 <kofi.taylor07@gmail.com>
2024-02-01 11:25:46 +0000

    setting up express middlewares and file storage

--- server/index.js
+++ server/index.js
@@ -8,3 +8,32 @@ import morgan from "morgan";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
+
+//CONFIGURATIONS
+
+const __filename = fileURLToPath(import.meta.url); // so we can grab file url specificall when we use modules
+const __dirname = path.dirname(__filename); // only when we use filename: for determining current file absolute path
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/asset", express.static(path.join(__dirname, 'public/assets')));
+
+// FILE STORAGE
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "public/assests");
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ storage })
+
+
