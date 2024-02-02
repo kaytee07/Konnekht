8086d73c9ab50edc3d7ced7240f6ac6de4a7e3f6
kaytee07 <kofi.taylor07@gmail.com>
2024-02-02 20:06:39 +0000

    create post controller created

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,1 +1,27 @@
-import Post from "../controllers/posts";
+import Post from "../models/Post";
+import User from "../models/User";
+
+
+/* CREATE */
+export const createPost = async (req, res) => {
+    try {
+        const { userId, description, picturePath, location } = req.body;
+        const user = await User.findById(userId);
+        const newPost = new Post({
+            userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            userPicturePath: user.picturePath,
+            picturePath,
+            description,
+            location,
+            likes: {},
+            comment: [],
+        })
+        await newPost.save()
+        const post = Post.find();
+        res.status(201).json(post);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
