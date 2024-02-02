ebb1efe16c79381d90a5497b3a0d76b9dd3edc7d
kaytee07 <kofi.taylor07@gmail.com>
2024-02-02 20:36:16 +0000

    like post controller created

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -27,7 +27,7 @@ export const createPost = async (req, res) => {
 
 };
 
-export const getFeedPosts = async () => {
+export const getFeedPosts = async (req, res) => {
     try {
         const feed = Post.find();
         res.status(200).json(feed);
@@ -36,12 +36,37 @@ export const getFeedPosts = async () => {
     }
 }
 
-export const getUserPosts = async () => {
+export const getUserPosts = async (req, res) => {
     try {
         const { userId } = req.params;
         const userPosts = Post.find({ userId });
         res.status(200).json(userPosts);
     } catch(error) {
          res.status(404).json({ message: error.message });
+    }
+}
+
+export const likePost = async () => {
+    try {
+        const { id } = req.params;
+        const { userId } = req.body;
+        const post = await Post.findById(id);
+        const isLiked = post.likes.get(userId);
+
+        if (isLiked) {
+            post.likes.delete(userId);
+        } else {
+            post.likes.set(userId, true);
+        }
+
+        const updatedPost  = await Post.findByIdAndUpdate(
+            id,
+            { likes: post.likes },
+            { new: true } 
+        );
+
+        res.status(200).json(updatedPost);
+    } catch {
+        res.status(404).json({ message: error.message });
     }
 }
