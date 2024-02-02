bfea434c12961afa5d07d8221d8af937d0db7c3c
kaytee07 <kofi.taylor07@gmail.com>
2024-02-02 19:50:56 +0000

    model for post has been created

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,17 @@
-import express from "express";
+import express, { Router } from "express";
 import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
 
 /* READ */
 const postRoutes = express.Router();
 postRoutes.get("/", verifyToken, getFeedPosts);
-postRoutes.get()
+postRoutes.get("/:userid/posts", verifyToken, getUserPosts);
+
 
 /* UPDATES */
+postRoutes.patch("/:id/like", verifyToken, likePost);
+
+export default postRoutes;
 
 
 
