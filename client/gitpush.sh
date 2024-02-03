e7026b75200afa2c6265602aad990c314edd5293
kaytee07 <kofi.taylor07@gmail.com>
2024-02-03 16:32:03 +0000

    script to push changes to github

--- client/gitpush.sh
+++ client/gitpush.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -lt 2 ]; then
     echo "Usage: $0 <filename> <commit_message>"
     exit 1
 fi
 
-filename=$1
-commit_message=$2
+commit_message="${!#}"
 
-git add "$filename"
+for ((i=1; i<$#; i++)); do
+    git add "${!i}"
+done
+
 git commit -m"$commit_message"
 git push
 
