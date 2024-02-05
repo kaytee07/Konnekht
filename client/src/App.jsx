0ba7cff0a89cb9bdf81ead276080c91c0c6e15f4
kaytee07 <kofi.taylor07@gmail.com>
2024-02-05 14:41:45 +0000

    material ui theme set

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,27 @@
 import { BrowserRouter,  Routes, Route } from "react-router-dom";
-import HomePage from "screens/homePage";
-import LoginPage from "screens/loginPage";
-import ProfilePage from "screens/profilePage";
+import HomePage from "./screens/homePage";
+import LoginPage from "./screens/loginPage";
+import ProfilePage from "./screens/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import {  ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+    const mode = useSelector((state) => state.mode);
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
     return (
         <div className="App">
             <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<LoginPage/>}/>
-                    <Route path="/home" element={<HomePage/>}/>
-                    <Route path="/profile/:userId:" element={<ProfilePage/>}/>
-                </Routes>
+                <ThemeProvider theme={theme}>    
+                    <Routes>
+                        <Route path="/" element={<LoginPage/>}/>
+                        <Route path="/home" element={<HomePage/>}/>
+                        <Route path="/profile/:userId:" element={<ProfilePage/>}/>
+                    </Routes>
+                </ThemeProvider>
             </BrowserRouter>
         </div>
     )
