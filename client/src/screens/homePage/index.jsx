d5e99732c429cec2301f148b043759e7fa8882c8
kaytee07 <kofi.taylor07@gmail.com>
2024-02-11 20:40:02 +0000

    layout

--- client/src/screens/homePage/index.jsx
+++ client/src/screens/homePage/index.jsx
@@ -1,9 +1,33 @@
-import { Box } from "@mui/material"
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 import Navbar from "../navbar";
+import UserWidget from "../../screens/widgets/UserWidget";
 const HomePage = () => {
+    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+    const {_id, picturePath } = useSelector((state) => state.user)
     return (
         <Box>
             <Navbar />
+            <Box
+              width="100%"
+              padding="2rem 6%"
+              display={isNonMobileScreens ? "flex" : "block"}
+              gap="0.5rem"
+              justifyContent="space-between"
+            >
+                <Box  flexBasis={isNonMobileScreens ? "25%" : undefined}>
+                 <UserWidget userId={_id} picturePath={picturePath}/>
+                </Box>
+                <Box  
+                 flexBasis={isNonMobileScreens ? "42%" : undefined}
+                 mt={isNonMobileScreens ? undefined : "2rem"}
+                >
+                {isNonMobileScreens && (
+                    <Box flexBasis="25%">
+                    </Box>
+                )}
+                </Box>
+            </Box>
         </Box>
     )
 }
--- client/src/screens/widgets/UserWidget.jsx
+++ client/src/screens/widgets/UserWidget.jsx
@@ -1,13 +1,14 @@
 import {
-    ManageAccountsOutlined,
+  //  ManageAccountsOutlined,
     EditOutlined,
     LocationOnOutlined,
+    ManageAccountsOutlined,
     WorkOutlineOutlined
 } from "@mui/icons-material";
 import { Box, Typography, Divider, useTheme, Stack, Grid} from "@mui/material";
 import UserImage from  "../../components/UserImage";
 import WidgetWrapper from "../../components/WidgetWrapper";
-import { useSelector } from "react-redux";
+//import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -16,12 +17,13 @@ const UserWidget = ({ userId, picturePath }) => {
     const [user, setUser] = useState(null);
     const { palette } = useTheme();
     const navigate = useNavigate();
-    const token = useSelector((state) => state.token);
-    const dark = palette.neutral.dark;
-    const medium = palette.neytral.medium;
+    // const token = useSelector((state) => state.token);
+    // const dark = palette.neutral.dark;
+    const medium = palette.neutral.medium;
     const main = palette.neutral.main;
 
     const getUser = async () => {
+        console.log(userId)
         const response = await fetch(`http://localhost:3001/users/${userId}`, {
             method: "GET",
             credentials: "include"
@@ -55,9 +57,11 @@ const UserWidget = ({ userId, picturePath }) => {
         <Grid container
             gap="0.5rem"
             pb="1.1rem"
+            display="flex"
+            flexDirection="column"
             onClick={() => navigate(`/profile/${userId}`)}
         >
-            <Grid item>
+            <Grid item display="flex" justifyContent="space-between">
                 <Stack direction="row">
                     <UserImage image={picturePath}>
 
@@ -85,19 +89,70 @@ const UserWidget = ({ userId, picturePath }) => {
                         </Box>
                     </Stack>
                 </Stack>
+                <ManageAccountsOutlined/>
             </Grid>
-            <Grid item>
-                <Box></Box>
-                <Box></Box>
+            <Divider/>
+            {/* SECOND ROW */}
+            <Grid item p="1rem 0">
+                <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+                    <LocationOnOutlined fontSize="large" sx={{ color: main }}/>
+                    <Typography color={medium}>{location}</Typography>
+                </Box>
+                <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+                    < WorkOutlineOutlined fontSize="large" sx={{ color: main }}/>
+                    <Typography color={medium}>{occupation}</Typography>
+                </Box>
             </Grid>
-            <Grid item>
-                <Box></Box>
-                <Box></Box>
+             <Divider/>
+            {/* THIRD ROW */}
+            <Grid item p="1rem 0">
+                <Box display="flex" gap="1rem" flexDirection="row">
+                    <Typography color={medium}> {`who's viewed your profile`}</Typography>
+                    <Typography color={main} fontWeight="500">
+                        {viewedProfile}
+                    </Typography>
+                </Box>
+                <Box display="flex" gap="1rem" flexDirection="row">
+                    <Typography color={medium}>Impressions of your post</Typography>
+                    <Typography color={main} fontWeight="500">
+                        {impressions}
+                    </Typography>
+                </Box>
             </Grid>
-            <Grid item>
+             <Divider/>
+            {/* FOURTH ROW */}
+            <Grid item p="1rem 0">
+                <Typography fontSize="1rem" color="main" fontWeight="500" mb="1rem">
+                    Social Profiles
+                </Typography>
+                <Stack direction="column">
+                    <Stack direction="row">
+                        <img src="../../assets/linkedin.png" alt="twitter" />
+                        <Stack direction="column">
+                            <Typography color={main} fontWeight="500">
+                                 Twitter
+                            </Typography>
+                            <Typography color={medium}>
+                                 Social Network
+                            </Typography>
+                        </Stack>
+                        <EditOutlined sx={{ color: main }}/>
+                    </Stack>
 
-            </Grid>
-           
+                    <Stack direction="row">
+                        <img src="../../public/linkedin.png" alt="twitter" />
+                        <Stack direction="column">
+                            <Typography color={main} fontWeight="500">
+                                 LinkedIn
+                            </Typography>
+                            <Typography color={medium}>
+                                 Network Platform
+                            </Typography>
+                        </Stack>
+                        <EditOutlined sx={{ color: main }}/>
+                    </Stack>
+                </Stack>
+            </Grid>   
         </Grid>
     </WidgetWrapper>
   )
