3ce345f3ca84f12d37dc80975820949bdc385ea7
kaytee07 <kofi.taylor07@gmail.com>
2024-02-15 11:32:28 +0000

    getPosts function form Post widget done

---
+++ client/src/screens/widgets/PostsWidget.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+
+const PostsWidget = () => {
+  return (
+    <div>PostsWidget</div>
+  )
+}
+
+export default PostsWidget;
+
