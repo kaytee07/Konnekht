a7fd0cd2da74d705936498c6b54cdfe4c4c4c7e1
kaytee07 <kofi.taylor07@gmail.com>
2024-02-15 12:05:24 +0000

    layout for grtPosts done

--- client/src/screens/widgets/PostsWidget.jsx
+++ client/src/screens/widgets/PostsWidget.jsx
@@ -1,10 +1,75 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { setPosts } from "../../state";
+import PostWidget from "./PostWidget";
 
 
-const PostsWidget = () => {
+const PostsWidget = ({ userId, isProfile = false }) => {
+    const dispatch = useDispatch();
+    const posts = useSelector((state) => state.posts);
+    const token = useSelector((state) => state.token);
+
+    const getPosts = async () => {
+        const response = await fetch("htpp://localhost:3001/posts", {
+            method: "GET",
+            credentials: "include"
+        });
+        const data = await response.json();
+        dispatch(setPosts({ posts: data }));
+    }
+
+    const getUserPosts = async () => {
+        const response = await fetch(`http://localhost:3001/posts/${userId}`, {
+            method: "GET",
+            credentials: "include"
+        });
+
+        const data = await response.json();
+        dispatch(setPosts({ posts: data}));
+    };
+
+    useEffect(() => {
+        if (isProfile) {
+            getUserPosts();
+        } else {
+            getPosts();
+        }
+    }, []);
+
   return (
-    <div>PostsWidget</div>
+    <>
+        {
+            posts.map(
+                ({
+                    _id
+                    userId,
+                    firstName,
+                    lastName,
+                    description,
+                    location,
+                    occupation,
+                    picturePath,
+                    userPicture,
+                    likes,
+                    comments
+                }) => (
+                    <PostWidget
+                        key={_id}
+                        postId={_id}
+                        postUserId={userId}
+                        firstName={firstName}
+                        lastName={lastName}
+                        description={description}
+                        location={location}
+                        occupation={occupation}
+                        picturePath={picturePath}
+                        userPicture={userPicture}
+                        likes={likes}
+                        comments={comments}
+                    />
+                )
+            )}
+    </>
   )
 }
 
